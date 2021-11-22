5fc77ad86f8eb81824ecc26338858b1661945acf
Lavi Jain <lavi.jain@espire.com>
2021-11-22 18:56:06 +0530

    commit

--- src/com/programming/FibonacciSeries.java
+++ src/com/programming/FibonacciSeries.java
@@ -1,5 +1,28 @@
 package com.programming;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
+	public static void main(String args[]) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		
+		int num1 = 1, num2 = 1, count;
+		count = scanner.nextInt();
+
+		System.out.println("Fibonacci series is:");
+
+		int i=1;
+		while (i<= count) {
+			System.out.println(num1 + "");
+			int sum = num1 + num2;                  //sum of prev two num
+			num1 = num2;
+			num2 = sum;
+			i++;
+
+		}
+
+	}
 
 }
