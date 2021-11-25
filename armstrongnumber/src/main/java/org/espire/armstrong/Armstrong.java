28f7778b4770c6abd2118ba3434a69b4f24ed1fc
Deebendu Kumar <deebendu.kumar@espire.com>
2021-11-25 19:51:42 +0530

    Updated Armstrong Number

--- armstrongnumber/src/main/java/org/espire/armstrong/Armstrong.java
+++ armstrongnumber/src/main/java/org/espire/armstrong/Armstrong.java
@@ -3,25 +3,46 @@ package org.espire.armstrong;
 import java.util.Scanner;
 
 public class Armstrong {
-	public static void main(String args[]) {
 
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the number: ");
+	/**
+	 *
+	 * @param value
+	 * @return
+	 */
+    public static String calculate(Integer value) {
+        int remainder, total = 0;
+        int orgnumber = value;
 
-		int num = scanner.nextInt();
-		int remainder, total = 0;
-		int orgnumber = num;
+        while (orgnumber != 0) {
+            remainder = orgnumber % 10;
+            total = total + remainder * remainder * remainder;
+            orgnumber = orgnumber / 10;
+        }
 
-		while (orgnumber != 0) {
-			remainder = orgnumber % 10;
-			total = total + remainder * remainder * remainder;
-			orgnumber = orgnumber / 10;
-		}
+        if (total == value)
+            return "Number is Armstrong";
+        else
+            return "Number is not Armstrong";
+    }
 
-		if (total == num)
-			System.out.println("Number is Armstrong");
-		else
-			System.out.println("Number is not Armstrong");
-	}
+    public static void main(String args[]) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+
+        int num = scanner.nextInt();
+        int remainder, total = 0;
+        int orgnumber = num;
+
+        while (orgnumber != 0) {
+            remainder = orgnumber % 10;
+            total = total + remainder * remainder * remainder;
+            orgnumber = orgnumber / 10;
+        }
+
+        if (total == num)
+            System.out.println("Number is Armstrong");
+        else
+            System.out.println("Number is not Armstrong");
+    }
 }
