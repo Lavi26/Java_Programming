b90b98fd07e6e4926743f0d773656caf4c53a249
Ankit Kumar Jaisawal <ak440414@gmail.com>
2021-12-12 15:52:16 +0530

    ankit

---
+++ Ankit/java_exercise/armstrongnumber/src/main/java/org/espire/armstrongnumber/App.java
@@ -0,0 +1,13 @@
+package org.espire.armstrongnumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
