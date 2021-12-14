29516bfd383ac768c7c9c60fcf00143a9ab889bb
Kajal <89968244+kajal233@users.noreply.github.com>
2021-12-14 18:00:33 +0530

    Delete kajal/java_exercises/vending_machine directory

--- kajal/java_exercises/vending_machine/src/main/java/org/espire/vendingmachine/InsufficientCreditException.java
+++
@@ -1,9 +0,0 @@
-package org.espire.vendingmachine;
-
-public class InsufficientCreditException extends RuntimeException {
-
-	public InsufficientCreditException(int credit, Product product) {
-		super(String.format("Credit of %d is insufficient to purchase %s. Insert more coins!", credit, product));
-	}
-
-}
--- kajal/java_exercises/vending_machine/src/main/java/org/espire/vendingmachine/OutOfStockException.java
+++
@@ -1,9 +0,0 @@
-package org.espire.vendingmachine;
-
-public class OutOfStockException extends RuntimeException {
-
-	public OutOfStockException(Product product) {
-		super(String.format("Out of stock of %s", product));
-	}
-
-}
