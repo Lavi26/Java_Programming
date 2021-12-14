58b430576c5f1d0deebe2db3a724014cd0a314e4
Kajal <89968244+kajal233@users.noreply.github.com>
2021-12-14 18:33:18 +0530

    Update ArmstrongNumber.java

--- kajal/java_exercises/armstrongnumber/src/main/java/org/espire/armstrong/ArmstrongNumber.java
+++ kajal/java_exercises/armstrongnumber/src/main/java/org/espire/armstrong/ArmstrongNumber.java
@@ -4,7 +4,7 @@ package org.espire.armstrong;
  * a program to check if the given number is Armstrong number or not
  */
 public class ArmstrongNumber {
-	
+	//main method
 	public static void main(String[] args) {
         int number = 153;
         if(isArmstrong(number)) {
@@ -13,7 +13,7 @@ public class ArmstrongNumber {
             System.out.println(number + " is an NOT Armstrong number!");
         }
     }
-     
+     //function to check if the number is Armstrong or not 
     public static boolean isArmstrong(int n) {
         int no_of_digits = String.valueOf(n).length();
         int sum = 0;
@@ -21,11 +21,16 @@ public class ArmstrongNumber {
         for(int i=1;i<=no_of_digits;i++) {
             int digit = value%10;
             value = value/10;
+	//calculates the power of a number up to digit times and add the resultant to the sum variable
+
             sum = sum + (int)Math.pow(digit, no_of_digits);
         }
+	    //compares the sum with n 
         if(sum == n) {
+		//returns true if sum and n are equal
             return true;
         }else {
+		//returns false if sum and n are not equal
             return false;
         }
     }
