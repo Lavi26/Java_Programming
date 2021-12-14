a607b0734d1d28aa79168db5b2b528a8797e6fbd
Kajal <89968244+kajal233@users.noreply.github.com>
2021-12-14 19:36:08 +0530

    Update PalindromeUsingLibrary.java

--- kajal/java_exercises/palindrome/src/main/java/org/espire/palindrome/PalindromeUsingLibrary.java
+++ kajal/java_exercises/palindrome/src/main/java/org/espire/palindrome/PalindromeUsingLibrary.java
@@ -4,9 +4,12 @@ package org.espire.palindrome;
  */
 public class PalindromeUsingLibrary {
 	public static boolean isPalindrome(String str){  
+		// create a StringBuilder object
 	    StringBuilder sb=new StringBuilder(str);  
+		 // reverse the string
 	    sb.reverse();  
 	    String rev=sb.toString();  
+		 //check if str is palindrome
 	    if(str.equals(rev)){  
 	        return true;  
 	    }
@@ -14,6 +17,7 @@ public class PalindromeUsingLibrary {
 	    {  
 	        return false;  
 	    }  }
+	//main method
 	    public static void main(String[] args) {  
 	        System.out.println(PalindromeUsingLibrary.isPalindrome("nitin"));
 	}  
