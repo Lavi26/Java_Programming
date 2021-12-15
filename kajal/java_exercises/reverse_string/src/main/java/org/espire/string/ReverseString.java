e1b001f0671087c6cdbfe851cd5f30a36f792865
Kajal <89968244+kajal233@users.noreply.github.com>
2021-12-15 10:43:48 +0530

    Update ReverseString.java

--- kajal/java_exercises/reverse_string/src/main/java/org/espire/string/ReverseString.java
+++ kajal/java_exercises/reverse_string/src/main/java/org/espire/string/ReverseString.java
@@ -1,9 +1,12 @@
 package org.espire.string;
 
 public class ReverseString {
+	// Function to reverse a string
 	public static String reverseString(String str){  
+	//Converting the string into a character array
 	    char ch[]=str.toCharArray();  
 	    String rev="";  
+	  //For loop to reverse a string
 	    for(int i=ch.length-1;i>=0;i--){  
 	        rev+=ch[i];  
 	    }  
