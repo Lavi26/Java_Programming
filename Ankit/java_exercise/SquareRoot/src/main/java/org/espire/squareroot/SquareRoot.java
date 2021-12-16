d50ea64609020e54bfcb69dfb3a97249d7d7d75f
Ankit Kumar Jaisawal <ak440414@gmail.com>
2021-12-16 20:24:28 +0530

    Java Exercise Ankit

--- Ankit/java_exercise/SquareRoot/src/main/java/org/espire/squareroot/SquareRoot.java
+++ Ankit/java_exercise/SquareRoot/src/main/java/org/espire/squareroot/SquareRoot.java
@@ -5,13 +5,29 @@ package org.espire.squareroot;
  *
  */
 public class SquareRoot {
-
-	/**
-	 * @param args
+	/*
+	 * Method to find the Square root of a number
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static double findSquareRoot(int n) {
+
+		double squareRoot, result;
+
+		// approximate by half of the given number
+		squareRoot = n / 2;
+
+		// until the difference of approximate value and root value is not equal to 0
+		do {
+			result = squareRoot;
+			squareRoot = (result + (n / result)) / 2;
+		} while ((result - squareRoot) != 0);
+
+		return squareRoot;
 
 	}
 
+	public static void main(String args[]) {
+
+		System.out.println(SquareRoot.findSquareRoot(3));
+
+	}
 }
---
+++ Ankit/java_exercise/permutation/src/test/java/org/espire/permutation/PermutationTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.espire.permutation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ankit.jaisawal
+ *
+ */
+public class PermutationTest {
+
+	/**
+	 * Test method for {@link org.espire.permutation.Permutation#findPermutation(java.lang.String, java.lang.Integer, java.lang.Integer)}.
+	 */
+	@Test
+	public void testFindPermutation1() {
+		
+	}
+
+}
