5dc1ef6d863e5094fbe06b430bdc98c2a0ad611b
Ankit Kumar Jaisawal <ak440414@gmail.com>
2021-12-20 12:24:39 +0530

    Java Exercise Ankit

--- Ankit/java_exercise/removeduplicates/src/test/java/org/espire/removeduplicates/RemoveDuplicatesTest.java
+++ Ankit/java_exercise/removeduplicates/src/test/java/org/espire/removeduplicates/RemoveDuplicatesTest.java
@@ -17,17 +17,21 @@ public class RemoveDuplicatesTest {
 	 * Test method for {@link org.espire.removeduplicates.RemoveDuplicates#removeDuplicateFromArray(E[])}.
 	 */
 	@Test
-	public void testRemoveDuplicateFromArray() {
-		Integer[] array = {1,3,4,5,7,67,67};
-		RemoveDuplicates.removeDuplicateFromArray(array);
-//		int[] sorted= {1,3,4,5,7,67,67};
-//		int[] sorted1= {1,3,4,5,7,67};
-//        RemoveDuplicateIntArray.removeDuplicate(sorted);
-//		//System.out.println(Arrays.toString(array));
-//		
-//		assertArrayEquals(sorted1,sorted);
-		
-		Integer[] expArray= {1,3,4,5,7,67};
-		assertArrayEquals(expArray, array);
+	public void testRemoveDuplicateFromArrayInteger() {
+
+		Integer integerArray[] = { 1, 1, 2, 3,3, 5, 9, 6 };
+		RemoveDuplicates.removeDuplicateFromArray(integerArray);
+	}
+	@Test
+	public void testRemoveDuplicateFromArrayString() {
+
+		String str[] = { "Abb", "Abb", "B", "C" };
+		RemoveDuplicates.removeDuplicateFromArray(str);
+	}
+	@Test
+	public void testRemoveDuplicateFromArrayCharacter() {
+
+		Character charArray[] = { 'a', 'a', 'B', 'C' };
+		RemoveDuplicates.removeDuplicateFromArray(charArray);
 	}
 }
