7608ca05f7b8bc9db5cfa034a7db94431922ae95
Lavi26 <89968738+Lavi26@users.noreply.github.com>
2021-12-21 22:49:31 +0530

    commit

---
+++ lavij/vending_machine/src/main/java/org/espire/machine/product/InventoryMapping.java
@@ -0,0 +1,17 @@
+package org.espire.machine.product;
+
+import java.util.HashMap;
+
+public class InventoryMapping {
+
+  private HashMap<Integer, Product> inventory = new HashMap<>();
+  
+  public InventoryMapping() {
+    
+  }
+  
+  public void add(Integer productId, Product product) {
+    
+  }
+  
+}
---
+++ lavij/vending_machine/src/main/java/org/espire/machine/product/Price.java
@@ -0,0 +1,10 @@
+package org.espire.machine.product;
+
+import lombok.Data;
+
+public class Price {
+
+  private Double mrp;
+  private Double price;
+
+}
